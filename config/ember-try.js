2907a22606a6b3e73268c9048b42abc84c8d2c3a
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-07-13 13:12:03 -0500

    Dependency: Version 0.2.3 -> 0.2.4
    Updated Ember-cli version to 1.13.1 and Ember version 1.13.3

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,9 +1,34 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-1.13.2',
+      name: 'default',
+      dependencies: { }
+    },
+    {
+      name: 'ember-release',
       dependencies: {
-        "ember": "1.13.2"
+        'ember': 'components/ember#release'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
+    },
+    {
+      name: 'ember-beta',
+      dependencies: {
+        'ember': 'components/ember#beta'
+      },
+      resolutions: {
+        'ember': 'beta'
+      }
+    },
+    {
+      name: 'ember-canary',
+      dependencies: {
+        'ember': 'components/ember#canary'
+      },
+      resolutions: {
+        'ember': 'canary'
       }
     }
   ]
